65ecb864398ee73a3259c14931b98eeccc521d73
Keeran Sivaneri <KSivaneri@ucvts.tec.nj.us>
2019-11-13 09:57:02 -0500

    finished exercise 1

--- src/sum.js
+++ src/sum.js
@@ -14,13 +14,12 @@ while (endLoop === false) {
     upperBound = Number(readlineSync.question("Upper bound: "));
 
     if (Number.isNaN(lowerBound) || Number.isNaN(upperBound)) {
-
+    }
+    else if (lowerBound == "" || upperBound == "") {
     }
     else if (lowerBound < MIN || lowerBound > MAX || upperBound < MIN || upperBound > MAX) {
-
     }
     else if (lowerBound >= upperBound) {
-
     }
     else {
         endLoop = true;
@@ -30,19 +29,19 @@ while (endLoop === false) {
 let lowerEvenBound = null;
 let upperEvenBound = null;
 
-if (lowerBound % 2 == 0) {
+if (lowerBound % 2 === 0) {
     lowerEvenBound = lowerBound;
 }
 else {
-    lowerEvenBound = lowerBound++
+    lowerEvenBound = lowerBound + 1;
 }
-if (upperBound % 2 == 0) {
+if (upperBound % 2 === 0) {
     upperEvenBound = upperBound;
 }
 else {
-    upperEvenBound = upperBound++
+    upperEvenBound = upperBound - 1;
 }
 
-const NUMBER_OF_EVENS = (upperEvenBound - lowerEvenBound) + 1;
-
-const SUM = (NUMBER_OF_EVENS / 2) * ((2 * lowerEvenBound) + (NUMBER_OF_EVENS - 1) * 2);
+const NUMBER_OF_EVENS = ((upperEvenBound - lowerEvenBound) / 2) + 1;
+const SUM = (NUMBER_OF_EVENS / 2) * (lowerEvenBound + upperEvenBound);
+console.log("\n" + SUM + ".");
